9cbaeb2263a0df94c48024c5cbad17060cb4d539
elnabittaj <elnabittaj@gmail.com>
2024-11-22 15:39:36 +0530

    Add files via upload

---
+++ triangle_pattern.py
@@ -0,0 +1,18 @@
+row=int(input("Enter number of rows"))
+for i in range(row):
+    for j in range (i+1):
+        print("*",end="")
+    print("")
+    
+row=int(input("enter number of rows"))
+for i in range (row):
+    for j in range (row-i):
+         print("*",end="")
+    print("")
+
+for i in range(1,row+1):
+    for j in range(row-i):
+        print(" ",end="")
+    for k in range(1,2*i):
+        print("*",end="")
+    print("")
