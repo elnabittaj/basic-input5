94c965da57e0bc8048d716e8d0c701663f055dce
elnabittaj <elnabittaj@gmail.com>
2024-11-30 15:48:00 +0530

    Add files via upload

---
+++ product_list.py
@@ -0,0 +1,16 @@
+'''author:Elna Ann Bittaj
+date:30/11/2024
+description:
+description:Write a Python function to sum all the numbers in a list'''
+def product_of_list(list):
+    product=1
+    for i in list:
+        product=product*i
+    print(product)
+list=[]
+num=int(input("enter the number of elements:"))
+for i in range(num):
+    n=int(input("Enter the number:"))
+    list.append(n)
+print(list)
+product_of_list(list)
