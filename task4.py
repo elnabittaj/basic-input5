82791e9ae76a56d76aa008ef9957d81116bcb33c
Elna Bittaj <elnabittaj@gmail.com>
2024-12-12 23:33:13 +0530

    Add files via upload

---
+++ task4.py
@@ -0,0 +1,13 @@
+'''
+author:Elna Ann Bittaj
+date:18-10-2024
+description:Python program that uses functions from the math module to perform the following operations on a number provided by the user:
+'''
+import math
+number1=int(input("enter a number"))
+number2=math.sqrt(number1)
+#print("square root of number is:", number2)
+number3=math.factorial(number1)
+#print("factorial of a number is:",number3)
+number4=math.pow(number1,2)
+print("power of a number is:",number4)
